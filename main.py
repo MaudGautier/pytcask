9ce23b52357ca9b996ad4216b5e43d2f5f584c7f
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-17 16:52:34 -0500

    refactor: add ActiveFile and ImmutableFile classes and hide details

--- main.py
+++ main.py
@@ -35,12 +35,21 @@ class File:
 
     def __init__(self, path: str, mode: str):
         self.file = open(path, mode=mode)
+        self.path = path
 
-    @property
-    def _current_offset(self) -> Offset:
-        return self.file.tell()
+    @staticmethod
+    def read(path: str, start: int, end: int):
+        with open(path, "r") as file:
+            file.seek(start)
+            value = file.read(end - start)
+            return value
 
-    def _write_to_file(self, item: DatabaseItem) -> Offset:
+
+class ActiveFile(File):
+    def __init__(self, path: str):
+        super().__init__(path=path, mode="w")
+
+    def _append(self, item: DatabaseItem) -> File.Offset:
         self.file.write(item.metadata)
         self.file.write(item.key)
         value_position_offset = self._current_offset
@@ -49,11 +58,28 @@ class File:
         self.file.flush()
         return value_position_offset
 
+    @property
+    def _current_offset(self) -> File.Offset:
+        return self.file.tell()
+
+    @property
+    def size(self) -> File.Offset:
+        return self._current_offset
+
+    def append(self, item: DatabaseItem) -> File.Offset:
+        return self._append(item=item)
+
     def close(self) -> None:
         self.file.close()
 
-    def append(self, item: DatabaseItem):
-        return self._write_to_file(item=item)
+    def convert_to_immutable(self, new_path: str) -> None:
+        self.file.close()
+        os.rename(src=self.path, dst=new_path)
+
+
+class ImmutableFile(File):
+    def __init__(self, path: str):
+        super().__init__(path=path, mode="r")
 
 
 class KeyDir:
@@ -64,7 +90,7 @@ class KeyDir:
     def __init__(self):
         self.content = {}
 
-    def update_key(
+    def update(
         self,
         key: DatabaseItem.Key,
         file_path: str,
@@ -75,6 +101,19 @@ class KeyDir:
             file_path=file_path, value_position=value_position, value_size=value_size
         )
 
+    def update_file_path(self, previous_path: str, new_path: str) -> None:
+        for key, key_dir_entry in self.content.items():
+            if key_dir_entry.file_path == previous_path:
+                self.update(
+                    key=key,
+                    file_path=new_path,
+                    value_position=key_dir_entry.value_position,
+                    value_size=key_dir_entry.value_size,
+                )
+
+    def get(self, key: DatabaseItem.Key):
+        return self.content[key] if key in self.content else None
+
 
 class Database:
     DEFAULT_DIRECTORY = "./datafiles/"
@@ -84,38 +123,24 @@ class Database:
     def __init__(self, directory=DEFAULT_DIRECTORY):
         self.directory = directory
         self.active_file_path = directory + "active.txt"
-        self.active_file = File(path=self.active_file_path, mode="w")
+        self.active_file = ActiveFile(path=self.active_file_path)
         self.key_dir = KeyDir()
 
+    def _generate_new_active_file(self):
+        immutable_file_path = f"{self.directory}{int(time())}.txt"
+        self.active_file.convert_to_immutable(new_path=immutable_file_path)
+        self.key_dir.update_file_path(
+            previous_path=self.active_file_path, new_path=immutable_file_path
+        )
+        self.active_file = ActiveFile(self.active_file_path)
+
     def _append_to_active_file(self, item: DatabaseItem) -> File.Offset:
-        key_value_metadata_line = item.metadata
-        active_file_size = self.active_file._current_offset
+        new_line_size = len(item.metadata) + item.key_size + item.value_size
+        expected_file_size = self.active_file.size + new_line_size
+        is_active_file_too_big = expected_file_size > Database.ACTIVE_FILE_THRESHOLD
 
-        if (
-            active_file_size
-            + len(key_value_metadata_line)
-            + item.key_size
-            + item.value_size
-            > Database.ACTIVE_FILE_THRESHOLD
-        ):
-            self.active_file.close()
-
-            # rename it
-            immutable_file_path = f"{self.directory}{int(time())}.txt"
-            os.rename(src=self.active_file_path, dst=immutable_file_path)
-
-            # Update the in-memory KEY_DIR
-            for key, key_dir_entry in self.key_dir.content.items():
-                if key_dir_entry.file_path == self.active_file_path:
-                    self.key_dir.update_key(
-                        key=key,
-                        file_path=immutable_file_path,
-                        value_position=key_dir_entry.value_position,
-                        value_size=key_dir_entry.value_size,
-                    )
-
-            # open the new active one
-            self.active_file = File(self.active_file_path, "w")
+        if is_active_file_too_big:
+            self._generate_new_active_file()
 
         value_position_offset = self.active_file.append(item)
         return value_position_offset
@@ -132,24 +157,26 @@ class Database:
         # TODO: think about a way to encapsulate these two in an atomic operation so that either both or none is performed!
         item = DatabaseItem(key=key, value=value)
         active_file_value_position_offset = self._append_to_active_file(item)
-        value_size = item.value_size
-        self.key_dir.update_key(
+        self.key_dir.update(
             key=key,
             file_path=self.active_file_path,
             value_position=active_file_value_position_offset,
-            value_size=value_size,
+            value_size=item.value_size,
         )
 
     def get(self, key: DatabaseItem.Key) -> DatabaseItem.Value or None:
-        """Returns the value for the key searched. If there is no such key in the database, return None."""
-        if key not in self.key_dir.content:
+        """Returns the value for the key searched.
+        If there is no such key in the database, returns None.
+        """
+        key_dir_entry = self.key_dir.get(key)
+        if not key_dir_entry:
             return None
 
-        key_dir_entry = self.key_dir.content[key]
-        with open(key_dir_entry.file_path, "r") as file_reader:
-            file_reader.seek(key_dir_entry.value_position)
-            value = file_reader.read(key_dir_entry.value_size)
-            return value
+        return File.read(
+            path=key_dir_entry.file_path,
+            start=key_dir_entry.value_position,
+            end=key_dir_entry.value_position + key_dir_entry.value_size,
+        )
 
 
 # ~~~~~~~~~~~~~~~~~~~
@@ -173,3 +200,6 @@ if __name__ == "__main__":
     searched_keys = ["key1", "key2", "key3", "key4"]
     for searched_key in searched_keys:
         print(f"Here is the value of {searched_key}", database.get(searched_key))
+
+    missing_key = "i_dont_exist"
+    print(f"Here is the value of {missing_key}", database.get(missing_key))
