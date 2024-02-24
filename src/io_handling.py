5b7831f7eb1d10d1a0c5429da46c6431376a31b5
Maud Gautier <maud.j.gautier@gmail.com>
2024-02-24 12:07:01 -0500

    feat: add __iter__ method on HintFile and decode HintFileItem

--- src/io_handling.py
+++ src/io_handling.py
@@ -217,6 +217,7 @@ class MergedFile(WritableFile):
         return file_key_dir
 
 
+# TODO: refactor HintFileItem and StoredItem (= DataFileItem) together ??? (many things similar)
 class HintFileItem:
     def __init__(
         self,
@@ -234,11 +235,38 @@ class HintFileItem:
     def __repr__(self):
         return f"{self.key}: {self.timestamp}-{self.key_size}-{self.value_size}-{self.value_position}"
 
-    def to_bytes(self):
-        metadata = struct.pack(
+    @property
+    def encoded_metadata(self) -> bytes:
+        return struct.pack(
             "iiii", self.timestamp, self.key_size, self.value_size, self.value_position
         )
+
+    @property
+    def encoded_key(self) -> bytes:
+        return bytes(self.key, encoding=ENCODING)
+
+    @property
+    def size(self):
+        return len(self.encoded_metadata) + len(self.encoded_key)
+
+    def to_bytes(self):
+        metadata = self.encoded_metadata
         return metadata + bytes(self.key, encoding=ENCODING)
+
+    @classmethod
+    def from_bytes(cls, data: bytes) -> "HintFileItem":
+        metadata_offset = 4 * NB_BYTES_INTEGER
+        timestamp, key_size, value_size, value_position = struct.unpack(
+            "iiii", data[:metadata_offset]
+        )
+        key = str(data[metadata_offset : metadata_offset + key_size], encoding=ENCODING)
+
+        return cls(
+            key=key,
+            value_size=value_size,
+            value_position=value_position,
+            timestamp=timestamp,
+        )
 
 
 class HintFile(File):
@@ -263,6 +291,19 @@ class HintFile(File):
                 key=key,
             )
             self.file.write(item.to_bytes())
+
+    # TODO: This is a close copy-paste of the File.__iter__ dunder method.
+    #  => Refactor with a generic ?? (StoredItem(DataFileItem) vs HintItem(HintFileItem))
+    def __iter__(self) -> Iterator[HintFileItem]:
+        file_size = os.path.getsize(self.path)
+        with open(self.path, "rb") as file:
+            data = file.read()
+            offset = 0
+            while offset < file_size:
+                item = HintFileItem.from_bytes(data[offset:])
+                chunk_size = item.size
+                offset += chunk_size
+                yield item
 
 
 class ActiveFile(WritableFile):
